f6f848eb4c9a7b3efdf70618af3dec07ba2a4c59
denstav <agressclash@gmail.com>
2022-04-19 12:08:50 +0300

    !

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let popupСall = document.querySelector('.popup-call')
+let openPopupСall = document.querySelectorAll('.call-btn')
+let closePopupCall = document.querySelector('.popup-close')
+
+console.log(closePopupCall)
+
+openPopupСall.forEach(element => {
+    element.addEventListener('click', function () {
+        popupСall.style.display = 'flex'
+    })
+});
+
+closePopupCall.addEventListener('click', function (event) {
+    event.preventDefault()
+    popupСall.style.display = 'none'
+
+    if (event.target.classList.contains('capture-form-btn')) {
+
+    }
+})
+
+
+let popupCheck = document.querySelector('.popup-check')
+let openCheckBtn = document.querySelector('.check-btn')
+openCheckBtn.addEventListener('click', function () {
+    popupCheck.style.display = 'flex'
+})
+
+
+closePopupCall.addEventListener('click', function (event) {
+    event.preventDefault()
+    popupCheck.style.display = 'none'
+})
+
+//Аккордеон
+const accordion = document.querySelectorAll('.panel-group');
+accordion.forEach((accordionEvent) => {
+    accordionEvent.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (event.target.closest('.panel-heading')) {
+            const allContent = accordionEvent.querySelectorAll('.panel-collapse');
+            allContent.forEach((content) => {
+                content.style.display = 'none';
+            })
+            let panel = event.target.closest('.panel');
+            let panelCollapse = panel.querySelector('.panel-collapse');
+            panelCollapse.style.display = 'block';
+        }
+    })
+})
